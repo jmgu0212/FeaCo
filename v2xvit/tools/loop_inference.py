c048c96d9ddf9d8c2da0a13876175c358f1b66c0
Jiaming Gu <971103961@qq.com>
2023-12-05 21:35:36 +0800

    shade some files

--- v2xvit/tools/loop_inference.py
+++
@@ -1,6 +0,0 @@
-import os   
-
-for index in range(15,40,2):
-  cmd = f"CUDA_VISIBLE_DEVICES=1 python /home/gaojing/zjy/v2x-vit/v2xvit/tools/inference.py --eval_epoch {index} "
-  print(f"Running command: {cmd}")
-  os.system(cmd)
--- v2xvit/version.py
+++
@@ -1,3 +0,0 @@
-"""Specifies the current version number of v2xvit."""
-
-__version__ = "0.1.0"
